f5189749bc6b73205e30dabdd415e7c7776e568f
tykowale <tykowale@gmail.com>
2017-11-27 19:55:22 -0600

    init

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Tyler Kowalewski on 11/27/17.
+//  Copyright © 2017 Tyler Kowalewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+This is my attempt at learning swift again...
+Following the site https://www.hackingwithswift.com/
+Let's see how this goes
+
